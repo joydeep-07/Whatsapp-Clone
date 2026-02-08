a012f3af8a82723435c47bcf278218d75c9b1a03
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-02-08 12:38:54 +0530

    Dashboard structure creating

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import SidePanel from './SidePanel'
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <div className='flex'>
+      <div className="left border h-screen w-1/4 ">
+      <SidePanel/>
+      </div>
+      <div className="right border h-screen w-3/4 "></div>
+    </div>
   )
 }
 
---
+++ frontend/src/components/SidePanel.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidePanel = () => {
+  return (
+    <div>
+      <div className="sidepanel bg-rose-300 h-screen w-15"></div>
+    </div>
+  );
+}
+
+export default SidePanel
