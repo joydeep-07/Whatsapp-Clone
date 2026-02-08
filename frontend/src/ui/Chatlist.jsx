cee11166e0ffce883340e25809ed235f813347ae
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-02-08 19:28:58 +0530

    User Instance Ui Created

--- frontend/src/ui/Chatlist.jsx
+++ frontend/src/ui/Chatlist.jsx
@@ -1,4 +1,4 @@
-import { CirclePlus, EllipsisVertical, Search } from "lucide-react";
+import { CheckCheck, CirclePlus, EllipsisVertical, Search, TicketX } from "lucide-react";
 import React from "react";
 
 const Chatlist = () => {
@@ -22,12 +22,40 @@ const Chatlist = () => {
           />
         </div>
 
-        <div className="badges flex gap-2">
-          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--accent-primary)]/50 text-xs font-medium border border-[var(--border-light)] " >All</button>
+        <div className="badges flex gap-2 mb-3">
+          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--accent-primary)]/50 text-xs font-medium border border-[var(--border-light)] ">
+            All
+          </button>
 
-          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--bg-secondary)]/50 text-xs font-medium border border-[var(--border-light)] " >Unread</button>
+          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--bg-secondary)]/50 text-xs font-medium border border-[var(--border-light)] ">
+            Unread
+          </button>
 
-          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--bg-secondary)]/50 text-xs font-medium border border-[var(--border-light)] " >Group</button>
+          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--bg-secondary)]/50 text-xs font-medium border border-[var(--border-light)] ">
+            Group
+          </button>
+        </div>
+
+        <div className="chatlist py-2 flex justify-start items-center gap-2">
+          <div className="profile">
+            <img
+              className="h-12 rounded-full"
+              src="https://i.pinimg.com/736x/19/1c/c9/191cc99599578fb10a08289d42471cad.jpg"
+              alt=""
+            />
+          </div>
+
+          <div className="name">
+            <h1 className="font-semibold">Hritik Roashan</h1>
+            <p className="text-xs flex gap-1 justify-start items-center">
+             
+              <span>
+               
+                <CheckCheck size={17} className="text-sky-500" />
+              </span>
+              Lorem ipsum dolor sit amet consectetur.
+            </p>
+          </div>
         </div>
       </div>
     </>
