6e56ea82a76df5e099bb27631508e3b5c6023051
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-02-08 19:14:24 +0530

    Filter Buttons Created

--- frontend/src/ui/Chatlist.jsx
+++ frontend/src/ui/Chatlist.jsx
@@ -5,7 +5,7 @@ const Chatlist = () => {
   return (
     <>
       <div className="h-screen p-4 flex flex-col">
-        <div className=" flex justify-between pb-4">
+        <div className=" flex justify-between mb-4">
           <h1 className="font-body text-xl tracking-wider">Chats</h1>
           <div className="flex gap-5 justify-center">
             <CirclePlus />
@@ -13,13 +13,21 @@ const Chatlist = () => {
           </div>
         </div>
 
-        <div className="flex items-center border pl-4 gap-2 border-[var(--border-light)] h-[40px] rounded-full overflow-hidden max-w-md w-full">
+        <div className="flex mb-3 items-center border pl-4 gap-2 border-[var(--border-light)] h-[40px] rounded-full overflow-hidden max-w-md w-full">
           <Search className="text-[var(--text-secondary)] " />
           <input
             type="text"
             placeholder="Search"
             className="w-full h-full outline-none text-[var(--text-secondary)] placeholder-gray-500 text-sm"
           />
+        </div>
+
+        <div className="badges flex gap-2">
+          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--accent-primary)]/50 text-xs font-medium border border-[var(--border-light)] " >All</button>
+
+          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--bg-secondary)]/50 text-xs font-medium border border-[var(--border-light)] " >Unread</button>
+
+          <button className="py-1 px-4 rounded-full text-[var(--text-main)] bg-[var(--bg-secondary)]/50 text-xs font-medium border border-[var(--border-light)] " >Group</button>
         </div>
       </div>
     </>
