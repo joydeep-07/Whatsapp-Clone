2da12eeb9ab03cfea1ab63e573273358f02947c8
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-02-08 19:07:24 +0530

    Ui Creating ....

--- frontend/src/ui/Chatlist.jsx
+++ frontend/src/ui/Chatlist.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { CirclePlus, EllipsisVertical, Search } from "lucide-react";
+import React from "react";
 
 const Chatlist = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-[var(--bg-secondary)]/70 ">
-      Chat list
-    </div>
-  );
-}
+    <>
+      <div className="h-screen p-4 flex flex-col">
+        <div className=" flex justify-between pb-4">
+          <h1 className="font-body text-xl tracking-wider">Chats</h1>
+          <div className="flex gap-5 justify-center">
+            <CirclePlus />
+            <EllipsisVertical />
+          </div>
+        </div>
 
-export default Chatlist
+        <div className="flex items-center border pl-4 gap-2 border-[var(--border-light)] h-[40px] rounded-full overflow-hidden max-w-md w-full">
+          <Search className="text-[var(--text-secondary)] " />
+          <input
+            type="text"
+            placeholder="Search"
+            className="w-full h-full outline-none text-[var(--text-secondary)] placeholder-gray-500 text-sm"
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Chatlist;
