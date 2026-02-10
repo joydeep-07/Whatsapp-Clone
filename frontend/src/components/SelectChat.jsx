d412c8f1ccf5dd3d41f47da98018ef1202b735c7
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-02-10 09:04:17 +0530

    Updated

--- frontend/src/components/SelectChat.jsx
+++ frontend/src/components/SelectChat.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import Lottie from "lottie-react";
+import email from "../assets/animations/email.json";
 
 const SelectChat = () => {
   return (
-    <div className="h-full flex items-center justify-center text-[var(--text-muted)]">
-      Select a chat to start messagingl
+    <div className="h-full flex flex-col items-center justify-center gap-4 text-[var(--text-muted)]">
+      {/* Lottie Animation */}
+      <div className="w-100 h-100">
+        <Lottie animationData={email} loop={true} />
+      </div>
+
+      {/* Text */}
+      <p className="text-2xl font-heading">
+        Select a chat to start <span className="text-[var(--accent-primary)] ">messaging</span>{" "}
+      </p>
     </div>
   );
-}
+};
 
-export default SelectChat
+export default SelectChat;
