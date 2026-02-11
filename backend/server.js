68dc578e49835f69ea40e91bd4ba5b67aeed5c3f
Joydeep Paul <joydeeprnp8821@gmail.com>
2026-02-11 11:28:35 +0530

    server started Database connected

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./db");
+
 const app = express();
-PORT = 3000;
+const PORT = 3000;
 
-app.get("/", (req, res)=>{
-    res.send("Hello World");
-})
+// Connect Database
+connectDB();
 
-app.listen(PORT, (err)=>{
-    if(err){
-        console.log(err);
-    }else{
-        console.log('SERVER STARTTED ON PORT ' + PORT)
-    }
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("SERVER STARTED ON PORT " + PORT);
+  }
+});
